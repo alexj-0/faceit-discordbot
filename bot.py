db66ff271fee85f8a139150079c6fde00ecb1329
Alex <alex80johnson@gmail.com>
2024-04-03 15:54:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,24 +5,27 @@ import json
 import datetime
 
 # Discord bot token
-TOKEN = 'BOT TOKEN'
+TOKEN = 'TOKEN' # REPLACE WITH YOUR BOT TOKEN
+CHANNEL_ID = 0 # REPLACE WITH YOUR CHANNEL ID
 
 # List of usernames
-usernames = ["USERNAME LIST"]
+usernames = []
 
 # Function to fetch data from the API
 async def fetch_data():
     results = {}
     for username in usernames:
-        url = f'https://api.satont.ru/faceit?nick={username}&view=%7Blvl%7D%2C%20%7Belo%7D&game=cs2'
+        url = f'http://api.faceit.myhosting.info:81/?n={username}'
         response = requests.get(url)
         if response.status_code == 200:
             try:
-                data = response.text.split(', ')  # Split the response by ', ' to extract level and elo
-                lvl = int(data[0])
-                elo = int(data[1])
-                results[username] = lvl, elo
-            except (IndexError, ValueError) as e:
+                data = json.loads(response.text)
+                level = data["level"]
+                elo = data["elo"]
+                trend = data["trend"]
+                elogain = data["today"]["elo"]
+                results[username] = level, elo, trend, elogain
+            except (KeyError, ValueError) as e:
                 print(f"Error parsing response for user {username}: {e}. Response content: {response.content}")
     return results
 
@@ -32,39 +35,55 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-# Function to update leaderboard every 5 minutes
+# Function to update leaderboard every 3 minutes
 def format_leaderboard_embed(results):
-    embed = discord.Embed(title="CS2 FACEIT Leaderboard", color=discord.Color.orange())
+    embed = discord.Embed(title="CS2 FACEIT Leaderboard", color=discord.Color.orange(), description="A leaderboard to show FACEIT recent match trend, elo and today's difference.")
     emoji_ids = {
-        10: 1223739013207166996,
-        9: 1223739012137750699,
-        8: 1223739010665414769,
-        7: 1223739009667170364,
-        6: 1223739008220270813
+        # REPLACE EACH 0 WITH EMOJI ID
+        10: 0,
+        9: 0,
+        8: 0,
+        7: 0,
+        6: 0,
+        5: 0,
+        4: 0,
+        3: 0,
+        2: 0,
+        1: 0 
     }
     sorted_results = sorted(results.items(), key=lambda x: x[1][1], reverse=True)  # Sort by elo rating (index 1)
-    for username, (lvl, elo) in sorted_results:
+
+    # Get len of longest elo gain to generate padding after
+    try:
+        highest_elogain = max(len(str(item[-1])) for _, item in sorted_results)
+        print(highest_elogain)
+    except:
+        highest_elogain = 1
+
+    for username, (level, elo, trend, elogain) in sorted_results:
         padding = " " * (30 - len(username))  # Calculate padding to make the length of username 30 characters
-        emoji_id = emoji_ids.get(lvl, None)
+        padding2 = " " * (highest_elogain - len(str(elogain)))
+        emoji_id = emoji_ids.get(int(level), None)
         if emoji_id:
-            emoji = f"<:{lvl}:{emoji_id}>"
+            emoji = f"<:{level}:{emoji_id}>"
         else:
             emoji = ""
+        trend = trend.replace('W', '🟢').replace('L', '🔴')
         # Create a clickable username using Discord's markdown syntax for hyperlinks
-        embed.add_field(name=f"<:{lvl}:{emoji_id}> ```{username}{padding}{elo}```", value="", inline=False)
+        embed.add_field(name=f"<:{level}:{emoji_id}> ```{username}{padding}{trend} • {elo} ({elogain}){padding2}``` ", value="", inline=False)
 
     # Add footer with timestamp in BST (UTC+1)
     timestamp_utc = datetime.datetime.utcnow()
     timestamp_bst = timestamp_utc + datetime.timedelta(hours=1)  # Convert UTC to BST
-    timestamp = timestamp_bst.strftime("%Y-%m-%d %H:%M:%S")
-    embed.set_footer(text=f"Last updated: {timestamp} UTC+1")
+    timestamp = timestamp_bst.strftime("%b %d, %Y - %H:%M:%S")
+    embed.set_footer(text=f"Last updated: {timestamp} (UTC+1)", icon_url="https://play-lh.googleusercontent.com/4iFS-rI0ImIFZyTwjidPChDOTUGxZqX2sCBLRsf9g_noMIUnH9ywsCmCzSu9vSM9Jg")
 
     return embed
 
 # Function to update leaderboard every minute
 async def update_leaderboard():
     await client.wait_until_ready()
-    channel = client.get_channel("CHANNEL ID")  # Replace your_channel_id_here with actual channel ID
+    channel = client.get_channel(CHANNEL_ID)  # Replace your_channel_id_here with actual channel ID
     leaderboard_message = None  # Variable to store the leaderboard message
     while not client.is_closed():
         results = await fetch_data()
@@ -77,13 +96,20 @@ async def update_leaderboard():
             # Edit the existing message with the updated leaderboard and timestamp
             await leaderboard_message.edit(embed=leaderboard_embed)
 
-        await asyncio.sleep(300)  # 60 seconds = 1 minute
-
+        await asyncio.sleep(180)  # 60 seconds = 1 minute
 
 # Event: Bot is ready
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    channel = client.get_channel(CHANNEL_ID)  # Replace your_channel_id_here with actual channel ID
+
+    # Check for existing messages in the channel
+    async for message in channel.history(limit=None):
+        if message.author == client.user:
+            # If the message is sent by the bot, delete it
+            await message.delete()
+
     client.loop.create_task(update_leaderboard())
 
 # Run the bot
@@ -91,3 +117,4 @@ async def main():
     await client.start(TOKEN)
 
 asyncio.run(main())
+
